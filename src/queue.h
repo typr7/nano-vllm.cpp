a3917deb41aeb721bfa95d617032906f8e6709d5
typr7 <blame773@gmail.com>
2026-07-23 00:54:26 +0800

    Add block queue

---
+++ src/queue.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <stdexcept>
+#include <stop_token>
+#include <utility>
+
+
+namespace cllm
+{
+
+template <typename T>
+class Queue
+{
+public:
+    Queue() = default;
+
+    Queue(const Queue&) = delete;
+    Queue& operator=(const Queue&) = delete;
+
+    void push(T value)
+    {
+        {
+            const std::lock_guard lock(mutex_);
+            if (closed_) {
+                throw std::logic_error("Cannot push to a closed queue");
+            }
+            queue_.push(std::move(value));
+        }
+        condition_.notify_one();
+    }
+
+    std::optional<T> pop(std::stop_token stop_token = {})
+    {
+        std::unique_lock lock(mutex_);
+        condition_.wait(lock, stop_token, [this] {
+            return closed_ || !queue_.empty();
+        });
+
+        if (queue_.empty()) {
+            return std::nullopt;
+        }
+
+        T value = std::move(queue_.front());
+        queue_.pop();
+        return value;
+    }
+
+    void close() noexcept
+    {
+        {
+            const std::lock_guard lock(mutex_);
+            closed_ = true;
+        }
+        condition_.notify_all();
+    }
+
+private:
+    std::mutex mutex_;
+    std::condition_variable_any condition_;
+    std::queue<T> queue_;
+    bool closed_{false};
+};
+
+}
